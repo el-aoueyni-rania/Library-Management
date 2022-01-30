5c9fb92756e5d1a2a25508780d77ac33f5ea36cf
el-aoueyni-rania <elaoueynirania@gmail.com>
2022-01-30 19:35:22 +0100

    ...

--- resources/views/panelUser/allbookuser.blade.php
+++ resources/views/panelUser/allbookuser.blade.php
@@ -31,8 +31,11 @@
               </p>
             </div>
             <div class="card-footer">
+              @if ( $book -> total == 0)
+              <b style="color: red"> Currently unavailable </b>
+              @else
               <a style="color: black" href="{{ URL::route('empruntbook', [ $book -> book_id ] ) }}">Emprunter</a>
-            </div>  
+              @endif            </div>  
           </div>
         </div> 
         @endforeach
